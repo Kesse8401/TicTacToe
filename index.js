213b9399858050e8763793f12cea8ce6517dbd00
Agnes Nowak <kesse8401@hotmail.com>
2018-06-22 13:12:20 +0200

    small changes to css and js file #3

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const tileOnClickHandler = () => console.log('tile number');
 const square = () => {
     const tile = document.createElement('div');
     tile.addEventListener( 'click', tileOnClickHandler );
-    tile.className = 'cipka';
+    tile.className = 'field';
  	return tile;									
 };
