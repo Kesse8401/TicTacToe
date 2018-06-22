45fd5f212971936598c527b3c1bae8d26d2321cc
Agnes Nowak <kesse8401@hotmail.com>
2018-06-22 10:50:09 +0200

    Few final updates #3

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const whichTile = () => console.log('tile number');         //variable whichTile zostaje przypisany funkcji whichTile. 
-                                                            // Funkcja whichTile zwraca do konsoli string "tile number"
+const tileOnClickHandler = () => console.log('tile number');
+                                                                    
 
 const square = () => {
     const tile = document.createElement('div');
-    tile.addEventListener('onClick', whichTile );	
+    tile.addEventListener( 'click', tileOnClickHandler );
+    tile.className = 'cipka';
  	return tile;									
-};                                                  // variable square zostaje przypisany funkcji która tworzy variable tile                                                      // która tworzy element div ORAZ adds addEventListener event handler do
-                                                    // tile. W tym przypadku jest to onCLick i funkcja whichTile.
-                                                    // na końcu square zwraca wartość const tile
-
-
-
-/* Skąd whichTile ma wiedzieć który tile został clicknięty skoro nie ma zadnych tajli do klikniecia?
-   Nalezy podmienić string "tile number" na array providujący numery od 0-8 eby concole.log mógł wskazać które pole zostało kliknięte.
-   How to use appropriate pointer on roll over without css and html? */
-
+};
